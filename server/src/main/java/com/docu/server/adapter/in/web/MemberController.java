9f8d41ea60abc9bdc484200c2b44308c924c6c16
Sangmin Lee <62678386+SangMin90@users.noreply.github.com>
2024-01-26 00:34:08 +0900

    #3 Controller 추가

---
+++ server/src/main/java/com/docu/server/adapter/in/web/MemberController.java
@@ -0,0 +1,13 @@
+package com.docu.adapter.in.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/member/manage")
+    public String memberManagement() {
+        return "memberManage.html";
+    }
+}
