720c6a9240d0e5bb7e14eb8b1e8ca62aa84dd90b
Sangmin Lee <62678386+SangMin90@users.noreply.github.com>
2024-01-26 00:22:35 +0900

    close #1 환경설정 변경

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.0"
+	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -11,14 +11,30 @@ java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
 	implementation("org.mariadb.jdbc:mariadb-java-client")
+
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
+
 	testRuntimeOnly("com.h2database:h2")
 }
 
