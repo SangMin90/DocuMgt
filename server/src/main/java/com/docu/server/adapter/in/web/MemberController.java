458aac56adc24ff282954ff2ae8c25c0f570ae4d
Sangmin Lee <62678386+SangMin90@users.noreply.github.com>
2024-01-26 09:24:16 +0900

    #3 MemberController 패키지 수정

--- server/src/main/java/com/docu/server/adapter/in/web/MemberController.java
+++ server/src/main/java/com/docu/server/adapter/in/web/MemberController.java
@@ -1,4 +1,4 @@
-package com.docu.adapter.in.web;
+package com.docu.server.adapter.in.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
