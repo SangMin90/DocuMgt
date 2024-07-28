f6bb7730a3ded24a0d7d97cc1f681c9894a74200
taejong <taejong1086@gmail.com>
2024-07-29 06:50:26 +0900

    teds

---
+++ server/src/main/java/com/docu/server/adapter/out/persistence/notice/entity/NoticeTitleSearchEntity.java
@@ -0,0 +1,16 @@
+package com.docu.server.adapter.out.persistence.notice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoticeTitleSearchEntity {
+
+    private String noticeTitleName;
+
+}
--- server/src/main/java/com/docu/server/biz/notice/service/NoticeService.java
+++ server/src/main/java/com/docu/server/biz/notice/service/NoticeService.java
@@ -3,9 +3,13 @@ package com.docu.server.biz.notice.service;
 import com.docu.server.biz.notice.port.in.NoticeInPort;
 import com.docu.server.biz.notice.port.out.NoticeOutPort;
 import com.docu.server.domain.common.ResponseMessage;
+import com.docu.server.domain.keyword.KeywordRes;
 import com.docu.server.domain.notice.NoticeReq;
+import com.docu.server.domain.notice.NoticeRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +19,10 @@ public class NoticeService implements NoticeInPort {
 
     @Override
     public ResponseMessage getNoticeList(NoticeReq req) {
-        noticeOutPort.getNoticeList(req);
-        return null;
+
+
+        List<NoticeRes> res = noticeOutPort.getNoticeList(req);
+
+        return ResponseMessage.builder().data(res).statusCode(200).build();
     }
 }
