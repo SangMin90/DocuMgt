4808c9f4f0d754bd9330d2420bda7be18ba3cee4
smLee <sm.lee@care-biz.co.kr>
2024-10-29 22:45:18 +0900

    스프링 시큐리티 초기 설정

--- server/src/main/java/com/docu/server/config/security/SecurityConfig.java
+++ server/src/main/java/com/docu/server/config/security/SecurityConfig.java
@@ -1,2 +1,24 @@
-package com.docu.server.config.security;public class SecurityConfig {
+package com.docu.server.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        authorize -> authorize.anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
 }
